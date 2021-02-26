f55d49ab3505f0e2cdab3be170d04d3b44299933
guo718 <60735779+guo718@users.noreply.github.com>
2021-02-26 16:50:33 +0800

    6

--- tempppp.cpp
+++ tempppp.cpp
@@ -1,102 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class temp
-{
-    int data;
-    int n;
-    int *p;
-    const int r;
-    static int com;
-
-public:
-    temp(int data, int n, int *j, int u) : r(u)
-    {
-        this->data = data;
-        this->n = n;
-        p = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            p[i] = j[i];
-        }
-    }
-    temp() : r(0)
-    {
-        data = 0;
-        n = 0;
-        p = NULL;
-    }
-    temp(const temp &rm) : r(rm.r)
-    {
-        n = rm.n;
-        data = rm.data;
-        p = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            p[i] = rm.p[i];
-        }
-    }
-    virtual void show() const
-    {
-        for (int i = 0; i < n; i++)
-        {
-            cout << p[i] << " ";
-        }
-    }
-    virtual temp operator+(const temp &rm)
-    {
-        temp result(*this);
-        result.data = rm.data + data;
-        result.n = rm.n + n;
-        result.p = new int[result.n];
-        return result;
-    }
-    friend temp operator*(temp &tm1, temp &tm2);
-    virtual ~temp()
-    {
-        if (p)
-        {
-            delete[] p;
-        }
-    }
-};
-int temp::com = 0;
-temp operator*(temp &tm1, temp &tm2)
-{
-    temp result;
-    result.n = tm1.n * tm2.n;
-    result.data = tm1.data * tm2.data;
-    result.p = new int[result.n];
-}
-class temp2 : virtual public temp
-{
-    void show() const
-    {
-        cout << "666" << endl;
-    }
-};
-template <typename T>
-class temp3 : public temp2
-{
-    T *data;
-    int n;
-
-public:
-    temp3(T *data,int n)
-    {
-        this->n=n;
-        data=new T[n];
-        for(int i=0;i<n;i++){
-            this->data[i]=data[i];
-        }
-    }
-    void show() const
-    {
-        for (int i = 0; i < n; i++)
-        {
-            cout << data[i] << " ";
-        }
-    }
-};
 int main()
 {
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    for (int i = 0; i < n;i++){
+        cin >> a[i];
+    }
+    int temp = 0;
+    for (int i = 0; i < n;i++){
+        for (int j = i + 1; j < n;j++){
+            if(a[i]>a[j]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n;i++){
+        cout << a[i] << " ";
+    }
 }
